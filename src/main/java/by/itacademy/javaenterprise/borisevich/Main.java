f3773af6573e1199eecea4c33d69b2a209b26d85
ieronim777 <71116171+ieronim777@users.noreply.github.com>
2021-11-30 13:39:28 +0300

    191

--- src/main/java/by/itacademy/javaenterprise/borisevich/Main.java
+++ src/main/java/by/itacademy/javaenterprise/borisevich/Main.java
@@ -12,16 +12,28 @@ import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @Slf4j
 public class Main {
 
-    public static void main(String[] args) throws DAOException, SQLException, LiquibaseException {
-        DataSource ds = new DriverManagerDataSource("jdbc:mysql://localhost:3306/demodb?useSSL=false",
-              "xxx", "xxx");
+    public static void main(String[] args) throws  SQLException, LiquibaseException {
+
+        Properties props = new Properties();
+        try (InputStream in = Files.newInputStream(Path.of("target/classes/liquibase/liquibase.properties"))) {
+            props.load(in);
+        } catch (IOException e) {
+            log.info("Error with reading file liquibase.properties.", e);
+        }
+
+        DataSource ds = new DriverManagerDataSource(props.getProperty("url"),
+                props.getProperty("username"), props.getProperty("password"));
         JdbcConnection jdbcConnection = new JdbcConnection(ds.getConnection());
         Database database = DatabaseFactory.getInstance().
                 findCorrectDatabaseImplementation(jdbcConnection);
